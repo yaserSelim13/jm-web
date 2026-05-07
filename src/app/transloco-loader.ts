2746bb81f53d85d09f7546f19afc887eb4b93114
yaserselim <yasso.selim@icloud.com>
2026-05-07 20:26:55 +0200

    add translation busting

--- src/app/transloco-loader.ts
+++ src/app/transloco-loader.ts
@@ -3,11 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { TranslocoLoader } from '@jsverse/transloco';
 import { Observable } from 'rxjs';
 
+const translationVersion = '2026-05-07';
+
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
   constructor(private http: HttpClient) {}
 
   getTranslation(lang: string): Observable<Record<string, string>> {
-    return this.http.get<Record<string, string>>(`/i18n/${lang}.json`);
+    return this.http.get<Record<string, string>>(`/i18n/${lang}.json?v=${translationVersion}`);
   }
 }
