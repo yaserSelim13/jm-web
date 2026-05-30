c7b8eddd4ebed709da4f9327c9289b7845b2e1ed
yaserselim <yasso.selim@icloud.com>
2026-05-30 04:16:46 +0200

    new marketing

--- src/app/transloco-loader.ts
+++ src/app/transloco-loader.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { TranslocoLoader } from '@jsverse/transloco';
 import { Observable } from 'rxjs';
 
-const translationVersion = '2026-05-07';
+const translationVersion = '2026-05-30';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
